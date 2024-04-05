820f6faf08d33bfffc8f93f0f43dad26d525a986
Rani teddu <raniteddu44@gmail.com>
2024-04-05 16:50:57 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from blogs.models import Blog, Category
 
 def home(request):
-    return render(request, 'home.html')
+    featured_posts = Blog.objects.filter(is_featured=True)
+    non_featured_posts = Blog.objects.filter(is_featured=False, status='Published')
+    context = {
+        'featured_posts' : featured_posts,
+        'non_featured_posts' : non_featured_posts
+    }
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Blog, Category
 
-# Create your views here.
+
+def posts_by_category(request, category_id):
+    catagory = get_object_or_404(Category, pk=category_id)
+    posts = Blog.objects.filter(category=category_id)
+    context = {
+        'catagory': catagory,
+        'posts' : posts
+    }
+    return render(request, 'posts_by_category.html', context)
