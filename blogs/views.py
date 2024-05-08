a09bb568768758876c23f5776c199b17dc3b2ab6
Rani teddu <raniteddu44@gmail.com>
2024-05-08 13:08:05 +0530

    comment feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
-from .models import Blog, Category
+from .models import Blog, Category,Comment
 from django.db.models import Q
 
 
@@ -14,8 +15,20 @@ def posts_by_category(request, category_id):
 
 def blog(request,slug):
     single_blog = get_object_or_404(Blog, slug=slug)
+    if request.method =='POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
     context = {
-        'single_blog' : single_blog
+        'single_blog' : single_blog,
+        'comments' : comments,
+        'comment_count' : comment_count
     }
     return render(request, 'blog.html', context)
 
